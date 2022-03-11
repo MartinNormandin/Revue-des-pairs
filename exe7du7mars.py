0dd66bc7a50d77cc09b120bb66954061e9d2c084
Eddiemn <eddiemn@hotmail.com>
2022-03-11 12:28:50 -0500

    added comments

--- exe7du7mars.py
+++ exe7du7mars.py
@@ -1,7 +1,9 @@
-#Créer une calculatrice d'IMC demandant à l'utilisateur sa grandeur(en mètres), son poids(en kg). Retournez ensuite la catégorie dans laquelle se trouve la personne.
+#Créer une calculatrice d'IMC demandant à l'utilisateur sa grandeur(en mètres), son poids(en kg). Retournez ensuite la catégorie dans laquelle se 
+#trouve la personne.
 
-#
+# nom de la fonction (presentement: poids_taille) ne représente pas ce qu'elle fait
 def poids_taille(): 
+# ne pas avoir de ligne vide inutile entre les lignes de code de la fonction 
 
     poids = int(input("Entrer votre poids en kg: "))
     taille = float(input("Entre votre taille en metres: "))

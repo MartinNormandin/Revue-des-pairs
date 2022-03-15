83c5df6b44f73f0ef7a859797634199c74f217fa
Eddiemn <eddiemn@hotmail.com>
2022-03-15 11:42:15 -0400

    add revue des pairs

--- exe6sem4.py
+++ exe6sem4.py
@@ -1,15 +1,15 @@
 #Écrire un programme demandant un entier à un utilisateur et lui retournant sa parité. Ex.: Le nombre «4» est paire.
-
+# pas de raison d'avoir un espace entre parite, ()
 def parite ():
 
     nombre = int(input("Entrer un nombre entier : "))
-
+    # espace recommandé entre nombre,%,2
     if nombre%2 == 0:
         print ("Votre nombre est pair")
 
     else:
         print ("votre nombre est impair")
-
+    # pas obligé de mettre None si rien est retourné
     return None
-
+# 2 lignes vides entre le bloc des fonctions et le bloc d'instructions
 parite()

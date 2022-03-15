ce97e2594adbb055480057984d7f4acd0e77224b
Eddiemn <eddiemn@hotmail.com>
2022-03-15 11:59:34 -0400

    add corrections

--- ex7sem4.py
+++ ex7sem4.py
@@ -7,15 +7,14 @@ def nombre_premier():
 
     if nombre < 1 or nombre > 20:
         # éviter les espaces entre le nom de la fonction (ici: la fonction print) et les ()
-        print ("Nombre invalide, essayer de nouveau: ")
+        print("Nombre invalide, essayer de nouveau: ")
         # eviter les lignes plus que 80 caractères 
-    elif nombre == 2 or nombre == 3 or nombre == 5 or nombre == 7 or nombre == 11 or nombre == 13 or nombre == 17 or nombre == 19:
-        print ("Votre nombre est un nombre premier")
+    elif nombre == 2 or nombre == 3 or nombre == 5 or nombre == 7 or nombre == 11 or \
+         nombre == 13 or nombre == 17 or nombre == 19:
+        print("Votre nombre est un nombre premier")
 
     else:
-        print ("Votre nombre n'est pas un nombre premier")
-
-    return None
-
+        print("Votre nombre n'est pas un nombre premier")
+    
 
 nombre_premier()
